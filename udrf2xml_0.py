2f4001d86274d5e42cb44356470f372f2d142a1c
shonosukehida <shonosuke.kms@keio.jp>
2025-07-09 14:49:46 +0900

    update

--- udrf2xml_0.py
+++
@@ -1,4 +0,0 @@
-import mujoco
-
-model = mujoco.MjModel.from_xml_path("panda.urdf")  # 拡張子が.urdfでもOK
-mujoco.mj_saveLastXML("panda.xml", model)
--- udrf2xml_1.py
+++
@@ -1,7 +0,0 @@
-from dm_control import mjcf
-
-model = mjcf.from_path("panda.urdf")
-xml_str = mjcf.to_xml_string(model)
-
-with open("panda.xml", "w") as f:
-    f.write(xml_str)
