d30bf3064cbdcac41ab9be3cd4b20ee9e766babf
shonosukehida <shonosuke.kms@keio.jp>
2025-07-10 11:28:25 +0900

    update

--- pldm_envs/franka/utils.py
+++ pldm_envs/franka/utils.py
@@ -7,16 +7,21 @@ def get_xy_range_from_model():
         config = yaml.safe_load(f)
     MODEL_PATH = config["model_path"]
     model = mujoco.MjModel.from_xml_path(MODEL_PATH)
-    
-    left_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, "wall_left")
-    right_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, "wall_right")
-    bottom_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, "wall_bottom")
-    top_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, "wall_top")
 
-    x_min = model.geom_pos[left_id][0] - model.geom_size[left_id][0]
-    x_max = model.geom_pos[right_id][0] + model.geom_size[right_id][0]
-    y_min = model.geom_pos[bottom_id][1] - model.geom_size[bottom_id][1]
-    y_max = model.geom_pos[top_id][1] + model.geom_size[top_id][1]
+    def get_geom_bound(name, axis, sign):
+        geom_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, name)
+        if geom_id == -1:
+            raise ValueError(f"{name} geom not found in model!")
+        return model.geom_pos[geom_id][axis] + sign * model.geom_size[geom_id][axis]
+
+    y_max = get_geom_bound("wall_left", 1, -1)
+    y_min = get_geom_bound("wall_right", 1, +1)
+    x_min = get_geom_bound("wall_bottom", 0, +1)
+    x_max = get_geom_bound("wall_top", 0, -1)
+    
+    print('BOUND')
+    print('X', x_min, x_max)
+    print('Y', y_min, y_max)
 
     return (x_min, x_max), (y_min, y_max)
 
@@ -34,12 +39,14 @@ def franka_pixel_mapper(coords, image_size=64):
     # y_range = (-0.401, 0.401)
 
 
-    x = (coords[..., 0] - x_range[0]) / (x_range[1] - x_range[0]) * (image_size - 1)
-    y = (coords[..., 1] - y_range[0]) / (y_range[1] - y_range[0]) * (image_size - 1)
+    x = (coords[:, :, 0] - x_range[0]) / (x_range[1] - x_range[0]) * (image_size - 1)
+    y = (coords[:, :, 1] - y_range[0]) / (y_range[1] - y_range[0]) * (image_size - 1)
 
     # matplotlib 用に上下反転
     pixel = torch.stack([x, y], dim=-1)
-    pixel[..., 1] = image_size - 1 - pixel[..., 1]
+    pixel[:, :, 1] = image_size - 1 - pixel[:, :, 1]
+    
+    pixel[:, :, 0] = image_size - 1 - pixel[:, :, 0]
 
     return pixel
 
